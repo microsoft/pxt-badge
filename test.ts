5cb807b81e6a27ecb265db7c6b53d4622f2890e2
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-07-26 08:25:25 -0700

    added test

--- test.ts
+++ test.ts
@@ -3,3 +3,53 @@ badge.setName("Cortana")
 badge.setCompany("NA")
 badge.setLogo("NA", 3);
 badge.start();
+
+// user config
+badge.setLinkedIn("https://www.linkedin.com/in/xyz/")
+
+badge.setProgram({
+    days: [
+        {
+            title: "Day one",
+            weekday: "Monday",
+            monthday: 6
+        },
+        {
+            title: "Day two",
+            weekday: "Tuesday",
+            monthday: 7
+        },
+        {
+            title: "Day three",
+            weekday: "Wednesday",
+            monthday: 8
+        }
+    ],
+    sessions: [
+        {
+            name: "Yadi",
+            presenter: "Yada",
+            info: "Yoda",
+            location: "WSCC:Room 608",
+            startTime: 1500,
+            endTime: 1600
+        },
+        {
+            name: "Mosi",
+            presenter: "Masa",
+            info: "Miso.",
+            location: "WSCC:Room 402",
+            startTime: 1600,
+            endTime: 1630
+        }
+    ],
+    questions: [
+        {
+            text: "Did you like it?",
+            options: ["Yeah!", "Nah"]
+        }
+    ]
+})
+
+storyboard.loaderBootSequence.register();
+badge.start()
